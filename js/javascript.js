54100611e5da4895d5a654d456ef811efeee8640
damiaoifpb <56745435+damiaoifpb@users.noreply.github.com>
2020-11-26 20:55:25 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,33 +1,52 @@
-function mes(){
-	var data = new Date();
-	var mes = data.getMonth();          
-	var meses = [
-	  "Janeiro",
-	  "Fevereiro",
-	  "Março",
-	  "Abril",
-	  "Maio",
-	  "Junho",
-	  "Julho",
-	  "Agosto",
-	  "Setembro",
-	  "Outubro",
-	  "Novembro",
-	  "Dezembro"
-	];
-	
-
-	return meses[mes];
+function closeall(){
+		document.getElementById('divinicio').style.display= 'none';
+		document.getElementById('divrendas').style.display= 'none';
+		document.getElementById('divgastos').style.display= 'none';
+		document.getElementById('divrelatorio').style.display= 'none';
+		document.getElementById('divaddrendas').style.display= 'none';
+		document.getElementById('diveditrendas').style.display= 'none';
+		document.getElementById('divdelrendas').style.display= 'none';
+		document.getElementById('divaddgastos').style.display= 'none';
+		document.getElementById('diveditgastos').style.display= 'none';
+		document.getElementById('divdelgastos').style.display= 'none';
 }
-
-function mostra(div){
-	if (div=='gasto') {
-		document.getElementById(div).style.display='Block';
-		document.getElementById('inicio').style.display='none';
-		document.getElementById('renda').style.display='none';
-		document.getElementById('relatorio').style.display='none';
-		document.getElementById('addrenda').style.display='none';
-		document.getElementById('editrenda').style.display='none';
-		document.getElementById('delrenda').style.display='none';
-	}
+function divinicio(){
+		closeall();
+		document.getElementById('divinicio').style.display= 'block';	
+}
+function divrendas(){
+		closeall();
+		document.getElementById('divrendas').style.display= 'block';	
+}
+function divgastos(){
+		closeall();
+		document.getElementById('divgastos').style.display= 'block';	
+}
+function divrelatorio(){
+		closeall();
+		document.getElementById('divrelatorio').style.display= 'block';	
+}
+function divaddrendas(){
+		closeall();
+		document.getElementById('divaddrendas').style.display= 'block';	
+}
+function diveditrendas(){
+		closeall();
+		document.getElementById('diveditrendas').style.display= 'block';	
+}
+function divdelrendas(){
+		closeall();
+		document.getElementById('divdelrendas').style.display= 'block';	
+}
+function divaddgastos(){
+		closeall();
+		document.getElementById('divaddgastos').style.display= 'block';	
+}
+function diveditgastos(){
+		closeall();
+		document.getElementById('diveditgastos').style.display= 'block';	
+}
+function divdelgastos(){
+		closeall();
+		document.getElementById('divdelgastos').style.display= 'block';	
 }
