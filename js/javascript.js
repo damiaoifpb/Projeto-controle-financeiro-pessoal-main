8cdf6dd2f1d76b9dfe2b12e021a7b08b9bf63bc0
damiaoifpb <56745435+damiaoifpb@users.noreply.github.com>
2020-12-04 20:28:25 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -70,3 +70,122 @@ function cadRenda(descricao,valor,tipo){
 	divrendas();
 
 }
+
+
+//codigo localStorege de renda
+      if (localStorage.descrenda) {
+
+	document.getElementById('descrenda').value = localStorage.descrenda;
+
+}
+      if (localStorage.valorrenda) {
+	document.getElementById('valorrenda').value = localStorage.valorrenda;
+}
+      if (localStorage.checkrenda) {
+	document.getElementById('checkrenda').value = localStorage.checkrenda;
+}
+      if (localStorage.checkrenda2) {
+	document.getElementById('checkrenda2').value = localStorage.checkrenda2;
+}
+
+
+var adicionarRenda = function() {
+	var descrenda = document.getElementById('descrenda').value;
+	var valorrenda = document.getElementById('valorrenda').value;
+	var checkrenda = document.getElementById('checkrenda').value;
+	var checkrenda2 = document.getElementById('checkrenda2').value;
+	localStorage.setItem('descrenda', descrenda);
+	localStorage.setItem('valorrenda', valorrenda);
+	localStorage.setItem('checkrenda', checkrenda);
+	localStorage.setItem('checkrenda2', checkrenda2);
+};
+
+document.onchange = adicionarRenda ;
+
+//codigo localStorege de gastos
+      if (localStorage.descrenda) {
+
+	document.getElementById('desgastos').value = localStorage.desgastos;
+
+}
+      if (localStorage.valorrenda) {
+	document.getElementById('valor').value = localStorage.valorgasto;
+}
+      if (localStorage.checkrenda) {
+	document.getElementById('c1').value = localStorage.c1;
+}
+   
+      if (localStorage.checkrenda) {
+	document.getElementById('c2').value = localStorage.c2;
+}
+   
+
+      if (localStorage.checkrenda) {
+	document.getElementById('c3').value = localStorage.c4;
+}
+   
+
+      if (localStorage.checkrenda) {
+	document.getElementById('c4').value = localStorage.c4;
+}
+   
+
+var adicionarGasto = function() {
+	var desgastos = document.getElementById('desgastos').value;
+	var valorgasto = document.getElementById('valor').value;
+	var checkgasto = document.getElementById('c1').value;
+	var checkgasto2 = document.getElementById('c2').value;
+	var checkgasto = document.getElementById('c3').value;
+	var checkgasto2 = document.getElementById('c4').value;
+	localStorage.setItem('desgastos', desgastos);
+	localStorage.setItem('valor', valor);
+	localStorage.setItem('c1', c1);
+	localStorage.setItem('c2', c2);
+	localStorage.setItem('c3', c3);
+	localStorage.setItem('c4', c4);
+	
+};
+
+document.onchange = adicionarGasto;
+
+//adiciornaRendaTabela
+
+function cadPessoa( descricao, valor) {
+    pessoas = document.getElementById("rendafx");    
+    var qtdlLinhas = pessoas.rows.length;
+    var linha = pessoas.insertRow(qtdlLinhas);
+    var linhaParam;
+
+    var cellCodigo = linha.insertCell(0);
+    var celldescricao = linha.insertCell(2);
+    var cellvalor = linha.insertCell(3);
+    
+
+    cellCodigo.innerHTML = qtdlLinhas;
+    cellDescricao.innerHTML = descricao;
+    cellvalor.innerHTML = valor;
+    
+    preencheCamposForm();
+
+}
+
+//adiciornaGastosTabela
+
+function cadPessoagastos( descricao, valor, parcelas) {
+    pessoas = document.getElementById("gastovar");    
+    var qtdlLinhas = pessoas.rows.length;
+    var linha = pessoas.insertRow(qtdlLinhas);
+    var linhaParam;
+
+    var cellCodigo = linha.insertCell(0);
+    var celldescricao = linha.insertCell(2);
+    var cellvalor = linha.insertCell(3);
+    
+
+    cellCodigo.innerHTML = qtdlLinhas;
+    cellDescricao.innerHTML = descricao;
+    cellvalor.innerHTML = valor;
+    
+    preencheCamposForm();
+
+}
